062f1c9713f361f4ecdb464c7113ba41ff56b191
LeandervanAarde <200211@virtualwindow.co.za>
2022-05-03 22:14:10 +0200

    Remove function
    Users can now remove daily appointments

--- src/Components/SubComponents/Weekappointments.js
+++ src/Components/SubComponents/Weekappointments.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Col} from 'react-bootstrap';
 const Weekappointments = (props) => {
+
+    const removeAppointment = (e) =>{
+        e.currentTarget.parentElement.remove();
+    }
+
     return (
        <Col md={12} className="WAppointmentCon">
            <Col md={12} className="Header"> <h2 className='text-center'>{props.children}</h2></Col>
@@ -15,72 +20,72 @@ const Weekappointments = (props) => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        <tr className="dataRow">
                             <td>09 May 2022</td>
-                            <td>Mr. L van Aarde</td>
+                            <td>Mr. R van Aarde</td>
                             <td>Dr. G Makan</td>
-                            <td className="remove"> -REMOVE</td>
+                            <td className="remove" onClick={removeAppointment}> -REMOVE</td>
                         </tr>
-                        <tr>
+                        <tr className="dataRow">
                             <td>09 May 2022</td>
-                            <td>Mr. L van Aarde</td>
+                            <td>Mr. B van Aarde</td>
                             <td>Dr. G Makan</td>
-                            <td className="remove"> -REMOVE</td>
+                            <td className="remove" onClick={removeAppointment}> -REMOVE</td>
                         </tr>
-                        <tr>
+                        <tr className="dataRow">
                             <td>09 May 2022</td>
-                            <td>Mr. L van Aarde</td>
+                            <td>Mr. C van Aarde</td>
                             <td>Dr. G Makan</td>
-                            <td className="remove"> -REMOVE</td>
-                        </tr>
-
-                        <tr>
-                            <td>09 May 2022</td>
-                            <td>Mr. L van Aarde</td>
-                            <td>Dr. G Makan</td>
-                            <td className="remove"> -REMOVE</td>
+                            <td className="remove" onClick={removeAppointment}> -REMOVE</td>
                         </tr>
 
-                        <tr>
+                        <tr className="dataRow">
                             <td>09 May 2022</td>
-                            <td>Mr. L van Aarde</td>
+                            <td>Mr. G van Aarde</td>
                             <td>Dr. G Makan</td>
-                            <td className="remove"> -REMOVE</td>
+                            <td className="remove" onClick={removeAppointment}> -REMOVE</td>
                         </tr>
 
-                        <tr>
+                        <tr className="dataRow"> 
                             <td>09 May 2022</td>
-                            <td>Mr. L van Aarde</td>
+                            <td>Mr. O van Aarde</td>
                             <td>Dr. G Makan</td>
-                            <td className="remove"> -REMOVE</td>
+                            <td className="remove" onClick={removeAppointment}> -REMOVE</td>
                         </tr>
 
-                        <tr>
+                        <tr className="dataRow">
                             <td>09 May 2022</td>
                             <td>Mr. L van Aarde</td>
                             <td>Dr. G Makan</td>
-                            <td className="remove"> -REMOVE</td>
+                            <td className="remove" onClick={removeAppointment}> -REMOVE</td>
                         </tr>
 
-                        <tr>
+                        <tr className="dataRow">
                             <td>09 May 2022</td>
                             <td>Mr. L van Aarde</td>
                             <td>Dr. G Makan</td>
-                            <td className="remove"> -REMOVE</td>
+                            <td className="remove" onClick={removeAppointment}> -REMOVE</td>
                         </tr>
 
-                        <tr>
+                        <tr className="dataRow">
                             <td>09 May 2022</td>
                             <td>Mr. L van Aarde</td>
                             <td>Dr. G Makan</td>
-                            <td className="remove"> -REMOVE</td>
+                            <td className="remove" onClick={removeAppointment}> -REMOVE</td>
                         </tr>
 
-                        <tr>
+                        <tr className="dataRow">
                             <td>09 May 2022</td>
                             <td>Mr. L van Aarde</td>
                             <td>Dr. G Makan</td>
-                            <td className="remove"> -REMOVE</td>
+                            <td className="remove" onClick={removeAppointment}> -REMOVE</td>
+                        </tr>
+
+                        <tr className="dataRow">
+                            <td>09 May 2022</td>
+                            <td>Mr. L van Aarde</td>
+                            <td>Dr. G Makan</td>
+                            <td className="remove" onClick={removeAppointment}> -REMOVE</td>
                         </tr>
                     </tbody>
                 </table>
