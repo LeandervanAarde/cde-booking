9324d309ffe707cd3c9d558173dcc585ed2c23fd
LeandervanAarde <200211@virtualwindow.co.za>
2022-05-10 08:12:43 +0200

    Staff added to the doctors section
    Staff members have been added

--- src/Components/Doctors.js
+++ src/Components/Doctors.js
@@ -11,48 +11,194 @@ import Staff from './SubComponents/Staff';
 const Doctors = () => {
     return (
         <>
-        <Col md={{ span: 8, offset: 1 }} className="workingCon">
-            <SearchInput>
-                Search Patient...
-            </SearchInput>
-            <h2 className='headingTwo'> CDE Program patients</h2>
+            <Col md={{ span: 8, offset: 1 }} className="workingCon">
+                <SearchInput>
+                    Search Patient...
+                </SearchInput>
+                <h2 className='headingTwo'> CDE Program patients</h2>
 
-            <Patientoverview 
-                title="Total Staff members"
-                number={"50"}
-                icon={<FaUser color={"#2663d4"} size={100} />} />
+                <Patientoverview
+                    title="Total Staff members"
+                    number={"50"}
+                    icon={<FaUser color={"#2663d4"} size={90} />} />
 
-            <Patientoverview
-                title="Total Appointments"
-                number="60"
-                icon={<FaBookMedical color={"#2663d4"} size={100} />} />
+                <Patientoverview
+                    title="Total Appointments"
+                    number="60"
+                    icon={<FaBookMedical color={"#2663d4"} size={90} />} />
 
-            <Patientoverview
-                title="Total Doctors"
-                number={"12"}
-                icon={<FaStethoscope color={"#2663d4"} size={100} />} />
+                <Patientoverview
+                    title="Total Doctors"
+                    number={"12"}
+                    icon={<FaStethoscope color={"#2663d4"} size={90} />} />
 
-            <h2 className='allPatients ms-2 mt-4'>All Receptionists</h2>
+                <h2 className='allPatients ms-2 mt-4'>All Receptionists</h2>
 
-            <Col md={{span:12}} className='staffWrapper'>
+                <Col md={{ span: 12 }} className='staffWrapper'>
 
-            <Staff/>
-            <Staff/>
-            <Staff/>
-            <Staff/>
-            <Staff/>
-            <Staff/>
+                    <Staff
+                        name="name:"
+                        nameContent="Dr. David Segal"
+                        Age="Age:"
+                        ageContent="54"
+                        sex="Sex:"
+                        sexContent="Male"
+                        cell="Cell:"
+                        cellContent="082 134 8492"
+                        mail="Mail:"
+                        mailContent="DavidS@CDE.com"
+                        unique="Room:"
+                        uniqueContent="D1"
+                        title="Specialisastion"
+                        titleContent="Endocronology"
+                        pay="Consult fee"
+                        payContent="R 650"
+
+                    />
+                    <Staff
+                        name="name:"
+                        nameContent="Dr. David Segal"
+                        Age="Age:"
+                        ageContent="54"
+                        sex="Sex:"
+                        sexContent="Male"
+                        cell="Cell:"
+                        cellContent="082 134 8492"
+                        mail="Mail:"
+                        mailContent="DavidS@CDE.com"
+                        unique="Room:"
+                        uniqueContent="D1"
+                        title="Specialisastion"
+                        titleContent="Endocronology"
+                        pay="Consult fee"
+                        payContent="R 650"
+
+                    />
+                    <Staff
+                        name="name:"
+                        nameContent="Dr. David Segal"
+                        Age="Age:"
+                        ageContent="54"
+                        sex="Sex:"
+                        sexContent="Male"
+                        cell="Cell:"
+                        cellContent="082 134 8492"
+                        mail="Mail:"
+                        mailContent="DavidS@CDE.com"
+                        unique="Room:"
+                        uniqueContent="D1"
+                        title="Specialisastion"
+                        titleContent="Endocronology"
+                        pay="Consult fee"
+                        payContent="R 650"
+
+                    />
+                    <Staff
+                        name="name:"
+                        nameContent="Dr. David Segal"
+                        Age="Age:"
+                        ageContent="54"
+                        sex="Sex:"
+                        sexContent="Male"
+                        cell="Cell:"
+                        cellContent="082 134 8492"
+                        mail="Mail:"
+                        mailContent="DavidS@CDE.com"
+                        unique="Room:"
+                        uniqueContent="D1"
+                        title="Specialisastion"
+                        titleContent="Endocronology"
+                        pay="Consult fee"
+                        payContent="R 650"
+
+                    />
+                    <Staff
+                        name="name:"
+                        nameContent="Dr. David Segal"
+                        Age="Age:"
+                        ageContent="54"
+                        sex="Sex:"
+                        sexContent="Male"
+                        cell="Cell:"
+                        cellContent="082 134 8492"
+                        mail="Mail:"
+                        mailContent="DavidS@CDE.com"
+                        unique="Room:"
+                        uniqueContent="D1"
+                        title="Specialisastion"
+                        titleContent="Endocronology"
+                        pay="Consult fee"
+                        payContent="R 650"
+
+                    />
+                    <Staff
+                        name="name:"
+                        nameContent="Dr. David Segal"
+                        Age="Age:"
+                        ageContent="54"
+                        sex="Sex:"
+                        sexContent="Male"
+                        cell="Cell:"
+                        cellContent="082 134 8492"
+                        mail="Mail:"
+                        mailContent="DavidS@CDE.com"
+                        unique="Room:"
+                        uniqueContent="D1"
+                        title="Specialisastion"
+                        titleContent="Endocronology"
+                        pay="Consult fee"
+                        payContent="R 650"
+
+                    />
+                    <Staff
+                        name="name:"
+                        nameContent="Dr. David Segal"
+                        Age="Age:"
+                        ageContent="54"
+                        sex="Sex:"
+                        sexContent="Male"
+                        cell="Cell:"
+                        cellContent="082 134 8492"
+                        mail="Mail:"
+                        mailContent="DavidS@CDE.com"
+                        unique="Room:"
+                        uniqueContent="D1"
+                        title="Specialisastion"
+                        titleContent="Endocronology"
+                        pay="Consult fee"
+                        payContent="R 650"
+
+                    />
+                    <Staff
+                        name="name:"
+                        nameContent="Dr. David Segal"
+                        Age="Age:"
+                        ageContent="54"
+                        sex="Sex:"
+                        sexContent="Male"
+                        cell="Cell:"
+                        cellContent="082 134 8492"
+                        mail="Mail:"
+                        mailContent="DavidS@CDE.com"
+                        unique="Room:"
+                        uniqueContent="D1"
+                        title="Specialisastion"
+                        titleContent="Endocronology"
+                        pay="Consult fee"
+                        payContent="R 650"
+
+                    />
+                </Col>
+
+
             </Col>
 
+            <Col md={3} className="work">
+                <Profile />
 
-        </Col>
+            </Col>
 
-        <Col md={3} className="work">
-            <Profile />
-
-        </Col>
-
-    </>
+        </>
     );
 };
 
--- src/Components/SubComponents/Staff.js
+++ src/Components/SubComponents/Staff.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import Primarybtn from './PrimaryBtn';
 
 // import Primarybtn from './PrimaryBtn';
 
 
 
-const Staff = () => {
-    return (
-    
-       <div className="staffInfo ">
-     <h2> Hello I need to be next to this </h2>
-       </div>
-    );
+const Staff = (props) => {
+  return (
+
+    <div className="staffInfo ">
+      <div>
+        <div className='staffPhoto'></div>
+        <p className='memberName'>Shooter McGavin</p>
+        <div>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <p className='name info'>{props.name} <span className='content'>{props.nameContent}</span></p>
+        <p className='dateOfBirth info'>{props.Age} <span className='content'>{props.ageContent}</span></p>
+        <p className='Gender info'>{props.sex} <span className='content'>{props.sexContent}</span></p>
+        <p className='phone info'>{props.cell} <span className='content'>{props.cellContent}</span></p>
+        <p className='email info'>{props.mail} <span className='content'>{props.mailContent}</span></p>
+        <p className='Room info'>{props.unique} <span className='content'>{props.uniqueContent}</span></p>
+        <p className='title info'>{props.title} <span className='content'>{props.titleContent}</span></p>
+        <p className='fee info'>{props.pay} <span className='content'>{props.payContent}</span></p>
+        <div className='removebtn'><Primarybtn id="removeStaff">- Remove</Primarybtn></div>
+      </div>
+
+    </div>
+  );
 };
 
 export default Staff;
